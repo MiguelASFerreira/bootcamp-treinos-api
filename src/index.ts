6085c232147c60fc2139ce94c24bc12863c82e19
MiguelASFerreira <migant1306@gmail.com>
2026-02-27 23:02:00 -0300

    chore: add better auth setup

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import "dotenv/config";
 
 import Fastify from "fastify";
-import fastifySwagger from '@fastify/swagger';
-import fastifySwaggerUI from '@fastify/swagger-ui';
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUI from "@fastify/swagger-ui";
 import {
   jsonSchemaTransform,
   serializerCompiler,
@@ -10,6 +10,8 @@ import {
   ZodTypeProvider,
 } from "fastify-type-provider-zod";
 import z from "zod";
+import { auth } from "./lib/auth.js";
+import fastifyCors from "@fastify/cors";
 
 const app = Fastify({
   logger: true,
@@ -21,9 +23,9 @@ app.setSerializerCompiler(serializerCompiler);
 await app.register(fastifySwagger, {
   openapi: {
     info: {
-      title: 'Bootcamp Treinos API',
-      description: 'API para gerenciamento de treinos e exercícios',
-      version: '1.0.0',
+      title: "Bootcamp Treinos API",
+      description: "API para gerenciamento de treinos e exercícios",
+      version: "1.0.0",
     },
     servers: [],
   },
@@ -31,8 +33,13 @@ await app.register(fastifySwagger, {
 });
 
 await app.register(fastifySwaggerUI, {
-  routePrefix: '/docs',
+  routePrefix: "/docs",
 });
+
+await app.register(fastifyCors, {
+  origin: ["http://localhost:3000"],
+  credentials: true,
+})
 
 app.withTypeProvider<ZodTypeProvider>().route({
   method: "GET",
@@ -49,8 +56,43 @@ app.withTypeProvider<ZodTypeProvider>().route({
   handler: () => {
     return {
       message: "Hello world!",
+    };
+  },
+});
+
+app.route({
+  method: ["GET", "POST"],
+  url: "/api/auth/*",
+  async handler(request, reply) {
+    try {
+      // Construct request URL
+      const url = new URL(request.url, `http://${request.headers.host}`);
+
+      // Convert Fastify headers to standard Headers object
+      const headers = new Headers();
+      Object.entries(request.headers).forEach(([key, value]) => {
+        if (value) headers.append(key, value.toString());
+      });
+      // Create Fetch API-compatible request
+      const req = new Request(url.toString(), {
+        method: request.method,
+        headers,
+        ...(request.body ? { body: JSON.stringify(request.body) } : {}),
+      });
+      // Process authentication request
+      const response = await auth.handler(req);
+      // Forward response to client
+      reply.status(response.status);
+      response.headers.forEach((value, key) => reply.header(key, value));
+      reply.send(response.body ? await response.text() : null);
+    } catch (error) {
+      app.log.error(error);
+      reply.status(500).send({
+        error: "Internal authentication error",
+        code: "AUTH_FAILURE",
+      });
     }
-  }
+  },
 });
 
 try {
